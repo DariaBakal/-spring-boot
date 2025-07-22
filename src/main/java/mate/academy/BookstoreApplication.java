a159a1330daa058634ce54f1cb5e8fea820d762e
DariaBakal <bakal.dasha01@gmail.com>
2025-07-22 16:56:10 +0300

    implemented Book, BookRepository, and BookService classes

---
+++ src/main/java/mate/academy/BookstoreApplication.java
@@ -0,0 +1,36 @@
+package mate.academy;
+
+import java.math.BigDecimal;
+import mate.academy.model.Book;
+import mate.academy.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication()
+public class BookstoreApplication {
+    @Autowired
+    private BookService bookService;
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookstoreApplication.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner commandLineRunner() {
+        return args -> {
+            Book testBook = new Book();
+            testBook.setTitle("The Clockmaker's Secret");
+            testBook.setAuthor("Eliza Thornton");
+            testBook.setIsbn("978-1-23456-789-7");
+            testBook.setPrice(new BigDecimal("18.99"));
+            testBook.setDescription("A gripping historical mystery set in Victorian London,"
+                    + " uncovering a hidden family legacy.");
+            testBook.setCoverImage("https://example.com/images/clockmaker.jpg");
+            bookService.save(testBook);
+            System.out.println(bookService.findAll());
+        };
+    }
+}
---
+++ src/main/java/mate/academy/config/AppConfig.java
@@ -0,0 +1,49 @@
+package mate.academy.config;
+
+import java.util.Properties;
+import javax.sql.DataSource;
+import org.apache.commons.dbcp2.BasicDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+
+@Configuration
+@PropertySource("classpath:application.properties")
+@ComponentScan(basePackages = "mate.academy")
+public class AppConfig {
+
+    private final Environment environment;
+    @Autowired
+    public AppConfig(Environment environment) {
+        this.environment = environment;
+    }
+
+    @Bean
+    public DataSource getDataSource() {
+        BasicDataSource dataSource = new BasicDataSource();
+        dataSource.setDriverClassName(environment.getProperty("spring.datasource.driver"));
+        dataSource.setUrl(environment.getProperty("spring.datasource.url"));
+        dataSource.setUsername(environment.getProperty("spring.datasource.username"));
+        dataSource.setPassword(environment.getProperty("spring.datasource.password"));
+        return dataSource;
+    }
+
+    @Bean(name = "entityManagerFactory")
+    public LocalSessionFactoryBean getSessionFactory() {
+        LocalSessionFactoryBean localSessionFactoryBean = new LocalSessionFactoryBean();
+        localSessionFactoryBean.setDataSource(getDataSource());
+
+        Properties properties = new Properties();
+        properties.put("show_sql", environment.getProperty("spring.jpa.show_sql"));
+        properties.put("hibernate.hbm2ddl.auto", environment
+                .getProperty("spring.jpa.hibernate.hbm2ddl.auto"));
+        localSessionFactoryBean.setHibernateProperties(properties);
+
+        localSessionFactoryBean.setPackagesToScan("mate.academy.model");
+        return localSessionFactoryBean;
+    }
+}
