1889ec6d092d654f2c7fef2dbe819e3c1578bf1e
DariaBakal <bakal.dasha01@gmail.com>
2025-07-28 18:53:14 +0300

    implemented BookDto, CreateBookRequestDto, BookController, and MapperConfig classes

--- src/main/java/com/example/bookstore/controller/BookController.java
+++ src/main/java/com/example/bookstore/controller/BookController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
 @RestController
-@RequestMapping("api/books")
+@RequestMapping("/books")
 public class BookController {
     private final BookService bookService;
 
@@ -25,7 +25,7 @@ public class BookController {
         return bookService.findAll();
     }
 
-    @GetMapping(value = "/{id}")
+    @GetMapping("/{id}")
     public BookDto getBookById(@PathVariable Long id) {
         return bookService.findById(id);
     }
