5f1ab886dcc4c3b12b6d36a8ab473b830c9f2561
Daria Bakal <111016269+DariaBakal@users.noreply.github.com>
2025-07-28 11:05:13 +0300

    implemented Book, BookRepository, and BookService classes (#1)
    * implemented Book, BookRepository, and BookService classes
    
    * implemented Book, BookRepository, and BookService classes
    
    * implemented Book, BookRepository, and BookService classes
    
    * implemented Book, BookRepository, and BookService classes
    
    * implemented Book, BookRepository, and BookService classes
    
    * implemented Book, BookRepository, and BookService classes

---
+++ src/main/java/com/example/bookstore/repository/BookRepositoryImpl.java
@@ -0,0 +1,47 @@
+package com.example.bookstore.repository;
+
+import com.example.bookstore.exception.DataProcessingException;
+import com.example.bookstore.model.Book;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class BookRepositoryImpl implements BookRepository {
+    private final SessionFactory sessionFactory;
+
+    @Override
+    public Book save(Book book) {
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = sessionFactory.openSession();
+            transaction = session.beginTransaction();
+            session.persist(book);
+            transaction.commit();
+            return book;
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new DataProcessingException("Can't add book to the DB: " + book, e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+    }
+
+    @Override
+    public List<Book> findAll() {
+        try (Session session = sessionFactory.openSession()) {
+            return session.createQuery("SELECT b from Book b", Book.class).getResultList();
+        } catch (Exception e) {
+            throw new DataProcessingException("Can't find any book in the DB", e);
+        }
+    }
+}
