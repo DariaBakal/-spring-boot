13a36f6122dfe0126958bea57ba75301d863833a
Daria Bakal <111016269+DariaBakal@users.noreply.github.com>
2025-08-15 16:03:29 +0300

    Pagination sorting swagger (#6)
    * implemented Book, BookRepository, and BookService classes
    
    * implemented Book, BookRepository, and BookService classes
    
    * implemented Book, BookRepository, and BookService classes
    
    * implemented Book, BookRepository, and BookService classes
    
    * implemented Book, BookRepository, and BookService classes
    
    * implemented Book, BookRepository, and BookService classes
    
    * implemented Book, BookRepository, and BookService classes
    
    * implemented BookDto, CreateBookRequestDto, BookController, and MapperConfig classes
    
    * implemented BookDto, CreateBookRequestDto, BookController, and MapperConfig classes
    
    * implemented BookDto, CreateBookRequestDto, BookController, and MapperConfig classes
    
    * implemented UpdateBookRequestDto and soft delete concept, added Liquibase support and missing endpoints and methods for BookService
    
    * implemented UpdateBookRequestDto and soft delete concept, added Liquibase support and missing endpoints and methods for BookService
    
    * implemented UpdateBookRequestDto and soft delete concept, added Liquibase support and missing endpoints and methods for BookService
    
    * implemented UpdateBookRequestDto and soft delete concept, added Liquibase support and missing endpoints and methods for BookService
    
    * implemented SpecificationBuilder, SpecificationProvider, SpecificationProviderManager, added different providers, added endpoint for search method
    
    * implemented SpecificationBuilder, SpecificationProvider, SpecificationProviderManager, added different providers, added endpoint for search method
    
    * implemented SpecificationBuilder, SpecificationProvider, SpecificationProviderManager, added different providers, added endpoint for search method
    
    * implemented CustomGlobalExceptionHandler class, added validations for image and isbn
    
    * implemented CustomGlobalExceptionHandler class, added validations for image and isbn
    
    * implemented CustomGlobalExceptionHandler class, added validations for image and isbn
    
    * deleted annotations,changed ImageValidator, enhanced IsbnValidator(turned out it was wrong)
    
    * added Pagination, Sorting and Swagger
    
    * added Pagination, Sorting and Swagger

--- src/main/java/com/example/bookstore/controller/BookController.java
+++ src/main/java/com/example/bookstore/controller/BookController.java
@@ -5,9 +5,12 @@ import com.example.bookstore.dto.BookSearchParameters;
 import com.example.bookstore.dto.CreateBookRequestDto;
 import com.example.bookstore.dto.UpdateBookRequestDto;
 import com.example.bookstore.service.BookService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
-import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,6 +22,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "Book management", description = "Endpoints for managing books")
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/books")
@@ -26,35 +30,49 @@ public class BookController {
     private final BookService bookService;
 
     @GetMapping
-    public List<BookDto> getAll() {
-        return bookService.findAll();
+    @Operation(summary = "Get all books",
+            description = "Get a paginated and sortable list of all available books")
+    public Page<BookDto> getAll(Pageable pageable) {
+        return bookService.findAll(pageable);
     }
 
     @GetMapping("/{id}")
+    @Operation(summary = "Get book by ID", description = "Get a book by its unique identifier")
     public BookDto getBookById(@PathVariable Long id) {
         return bookService.findById(id);
     }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
+    @Operation(summary = "Create a new book",
+            description = "Create a new book with the provided details")
     public BookDto createBook(@RequestBody @Valid CreateBookRequestDto bookDto) {
         return bookService.save(bookDto);
     }
 
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @DeleteMapping("/{id}")
+    @Operation(summary = "Delete book by ID",
+            description = "Delete a book from the database by its unique identifier")
     public void delete(@PathVariable Long id) {
         bookService.deleteById(id);
     }
 
     @PutMapping("/{id}")
+    @Operation(
+            summary = "Update an existing book",
+            description = "Update the details of an existing book by its unique identifier")
     public BookDto updateBook(@PathVariable Long id,
             @RequestBody @Valid UpdateBookRequestDto requestDto) {
         return bookService.updateBook(id, requestDto);
     }
 
     @GetMapping("/search")
-    public List<BookDto> searchBooks(BookSearchParameters searchParameters) {
-        return bookService.search(searchParameters);
+    @Operation(
+            summary = "Search books",
+            description = "Search for books by various parameters (e.g., title, author) with "
+                    + "pagination and sorting")
+    public Page<BookDto> searchBooks(BookSearchParameters searchParameters, Pageable pageable) {
+        return bookService.search(searchParameters, pageable);
     }
 }
