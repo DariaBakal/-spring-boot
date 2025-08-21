15d2e8316adbd557ccd750bf87510ee3261b9b44
DariaBakal <bakal.dasha01@gmail.com>
2025-08-21 21:26:13 +0300

    new logic, uncompleted

--- src/main/java/com/example/bookstore/repository/book/BookRepository.java
+++ src/main/java/com/example/bookstore/repository/book/BookRepository.java
@@ -4,7 +4,9 @@ import com.example.bookstore.model.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
+import java.util.List;
 
 @Repository
 public interface BookRepository extends JpaRepository<Book, Long>, JpaSpecificationExecutor<Book> {
+    List<Book> findAllByCategoryId(Long categoryId);
 }
